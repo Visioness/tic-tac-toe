4ed9406f159d91f81f58de400b989ebbba908353
Visioness <aygun-08-zurnaci@hotmail.com>
2025-03-10 15:09:25 +0300

    Add gameboard IIFE

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function createPlayer(name, marker, color) {
 }
 
 
+const gameboard = (function() {
+  const board = []
+  
+  const currentState = () => board;
+  
+  const create = () => {
+    for(let i = 0; i < 3; i++) {
+      const row = [];
+      for(let j = 0; j < 3; j++) {
+        row.push("null");
+      }
+      board.push(row);
+    }
+  }
+
+  const clear = () => {
+  }
+  
+  const move = (player, row, column) => board[row][column] = player.marker; 
+
+  return { create, currentState, clear, move };
+})();
+
+

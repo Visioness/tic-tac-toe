3ec6214a3a03e7239fc6a7da73c9049ba4ecd798
Visioness <aygun-08-zurnaci@hotmail.com>
2025-03-12 06:38:03 +0300

    Add draw X-O and update game

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function createPlayer(name, marker, color) {
   const getName = () => name;
   const getMarker = () => marker;
-  const changeName = (newName) => name = newName;
-  const changeColor = (newColor) => color = newColor;
+  const getColor = () => color;
  
-  return { getName, getMarker, changeName, changeColor };
+  return { getName, getMarker, getColor };
 }
 
 
@@ -52,49 +51,38 @@ const game = (function() {
   const players = [];
   let round;
   
-  const initialize = () => {
+  const start = (playerOne, playerTwo) => {
     // TEMP
-    const player1Info = {name:"Aygün", marker:"X", color:"crimson"};
-    const player2Info = {name:"Burak", marker:"O", color:"dodgerblue"}
-
-    const player1 = createPlayer(player1Info.name, player1Info.marker, player1Info.color);
-    const player2 = createPlayer(player2Info.name, player2Info.marker, player2Info.color);
-    
-    players.push(player1);
-    players.push(player2);
+    players.push(playerOne);
+    players.push(playerTwo);
 
     round = 0;
     
     gameboard.create();
+    console.log(playerOne.get);
+    console.log(`The game has been started between ${playerOne.getName()} and ${playerTwo.getName()}`);
   }
 
-  const play = () => {
-    initialize();
-
-    do {
-      nextRound();
-      playRound();
-    } while (!over());
-    
-    setTimeout(() => {
-      gameboard.clear();
-      reset();
-    }, 5000);
-  }
-
-  const playRound = () => {
-    gameboard.draw();
-    console.log(`${turn().getName()}'s turn!`);
-    const [row, column] = prompt("Row and column to mark (row,column)").split(",").map(s => Number(s.trim()));
-    
+  const playRound = (row, column) => {
     makeMove(row, column);
+    if (over()) {
+      // TEMP
+      console.log("Restarting in 2 seconds!")
+      return;
+    }
+    
+    nextRound();
   }
 
   const reset = () => {
     players.length = 0;
   }
   
-  const nextRound = () => round++;
+  const nextRound = () => {
+    round++;
+    gameboard.draw();
+    console.log(`${turn().getName()}'s turn!`);
+  }
 
   const turn = () => players[round % 2];
 
@@ -108,19 +96,19 @@ const game = (function() {
 
   const over = () => {
     if (checkWin()) {
-      console.log(`${turn().getName()} WINS`);
+      console.log(`${turn().getName()} WINS!`);
       return true;
     }
     
     if(!gameboard.currentState().flat().includes(null)) {
-      console.log("TIE");
+      console.log("TIE!");
       return true;
     }
 
     return false;
   }
 
-  return { play };
+  return { start, playRound, turn };
 })();
 
 const display = (function() {
@@ -130,7 +118,6 @@ const display = (function() {
   
   const initialize = () => {
     createBoard();
-    listenCellClicks();
     listenForms();
   }
   
@@ -150,7 +137,19 @@ const display = (function() {
   const listenCellClicks = () => {
     gameboardElement.addEventListener("click", (event) => {
       if (event.target.classList.contains("empty")) {
-        // TODO
+        let svg;
+        
+        if (game.turn().getMarker() == "x") {
+          svg = drawX(game.turn().getColor());
+        } else {
+          svg = drawO(game.turn().getColor());
+        }
+        
+        event.target.appendChild(svg);
+        event.target.classList.add(game.turn().getMarker());
+        event.target.classList.remove("empty");
+        
+        game.playRound(event.target.dataset.row, event.target.dataset.column);
         console.log(`Clicked cell ${event.target.dataset.row}-${event.target.dataset.column}`);
       }
     })
@@ -169,21 +168,77 @@ const display = (function() {
   const checkReady = () => {
     if ([playerOneForm, playerTwoForm].every(form => form.parentElement.classList.contains("ready"))) {
       [playerOneForm, playerTwoForm].forEach(form => {
-        form.parentElement.classList.add("removed");
-        form.parentElement.classList.remove("ready");
+        setTimeout(() => {
+          form.parentElement.classList.add("removed");
+          form.parentElement.classList.remove("ready");
+          listenCellClicks();
+        }, 2000);
       });
-    } else {
-      [playerOneForm, playerTwoForm].forEach(form => {
-        form.parentElement.classList.remove("removed");
-      });
+
+      game.start(
+        createPlayer(
+          playerOneForm.elements["player-name"].value,
+          playerOneForm.elements["marker-type"].value,
+          playerOneForm.elements["marker-color"].value
+        ),
+        createPlayer(
+          playerTwoForm.elements["player-name"].value,
+          playerTwoForm.elements["marker-type"].value,
+          playerTwoForm.elements["marker-color"].value
+        )
+      )
     }
   }
+
+  const drawX = (color) => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", "0 0 120 120");
+    svg.setAttribute("width", "120");
+    svg.setAttribute("height", "120");
+
+    const line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line1.classList.add("line");
+    line1.style.stroke = color;
+    line1.setAttribute("x1", "10");
+    line1.setAttribute("y1", "10");
+    line1.setAttribute("x2", "110");
+    line1.setAttribute("y2", "110");
+    
+    const line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line2.classList.add("line");
+    line2.style.stroke = color;
+    line2.setAttribute("x1", "110");
+    line2.setAttribute("y1", "10");
+    line2.setAttribute("x2", "10");
+    line2.setAttribute("y2", "110");
+
+    svg.appendChild(line1);
+    setTimeout(() => {
+      svg.appendChild(line2);
+    }, 1000)
+
+    return svg;
+  }
+
+  const drawO = (color) => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", "0 0 120 120");
+    svg.setAttribute("width", "120");
+    svg.setAttribute("height", "120");
+
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.classList.add("circle");
+    circle.style.stroke = color;
+    circle.setAttribute("cx", "60");
+    circle.setAttribute("cy", "60");
+    circle.setAttribute("r", "50");
+
+    svg.appendChild(circle);
+
+    return svg;
+  }
   
-  return { initialize };
+  return { initialize, drawX, drawO };
 })();
 
 display.initialize();
-/*
-game.play();
-*/
-
